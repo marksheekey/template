f4337e9e920a755b58641ff42e41fbddccf33676
marksheekey <marksheekey@hotmail.com>
2021-01-06 19:11:45 +0000

    Fetch

--- features/leave/repo/LeaveRepo.ts
+++ features/leave/repo/LeaveRepo.ts
@@ -7,24 +7,15 @@ export default class LeaveRepo {
         this.api = api
     }
     public async fetchLeave(startDate: string, endDate: string, callback: Callback) {
-        let leaveUI: LeaveUI[] = []
         callback.onLoading(true)
         await this.api.getLeave(startDate, endDate).catch((error) => {
             callback.onError(error)
         }).then((data) => {
                 if (data) {
-                    leaveUI = data.map(leaveUI)
-                    for (let leave of data) {
-                        let item:LeaveUI = {
-                            key : "key"+leave.id,
-                            start: leave.start_date,
-                            end: leave.end_date
-                        }
-                        leaveUI.push(item)
-                        console.log("leave:",leave.id)
-                    }
+                    callback.onResult(data.map( item => {return createLeaveUI(item)}))
+                }else{
+                    callback.onResult([])
                 }
-                callback.onResult(leaveUI)
             }
         ).finally( () => {
             callback.onLoading(false)
